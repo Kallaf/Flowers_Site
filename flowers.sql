38402e6b00819be47aebd8bf420ed99beed3e95d
Kallaf <kalaf1997@yahoo.com>
2019-11-03 04:12:26 +0200

    add backup from database

---
+++ flowers.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 03, 2019 at 03:11 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `flowers`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `flowers_name`
+--
+
+CREATE TABLE `flowers_name` (
+  `id` int(255) NOT NULL,
+  `flowers_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `flowers_name`
+--
+
+INSERT INTO `flowers_name` (`id`, `flowers_name`) VALUES
+(1, 'Jasmine'),
+(2, 'orchid'),
+(3, 'lotus');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `flowers_name`
+--
+ALTER TABLE `flowers_name`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
